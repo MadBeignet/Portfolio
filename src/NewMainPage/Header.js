ef703e01fd6dd538c653cfa33803f83169511861
LLJ3817 <maddiewisinski@northwesternmutual.com>
2023-06-13 15:55:48 -0500

    portfolio redo

---
+++ src/NewMainPage/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NewMainPage.css';
+import Hamburger from '../MainPage/Hamburger';
+
+function Header() {
+    return (
+        <div className="header">
+            <span id="fancy-name">Maddie Wisinski</span>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './MainPage/App';
+import App from './NewMainPage/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
