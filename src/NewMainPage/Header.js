cc5cd527dead80d110bf827defd2b58c00639010
LLJ3817 <maddiewisinski@northwesternmutual.com>
2023-06-13 16:06:11 -0500

    header work

--- src/NewMainPage/Header.js
+++ src/NewMainPage/Header.js
@@ -5,7 +5,7 @@ import Hamburger from '../MainPage/Hamburger';
 function Header() {
     return (
         <div className="header">
-            <span id="fancy-name">Maddie Wisinski</span>
+            <div className="name-box">Maddie Wisinski</div>
         </div>
     )
 }
