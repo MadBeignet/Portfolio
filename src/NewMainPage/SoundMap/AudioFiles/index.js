8dd155ac9a15a9ec1bc37cdf90e0c01eafa2a9a6
MadBeignet <maddie.wisinski@ymail.com>
2024-05-03 19:16:05 -0500

    created a sound map for city park

---
+++ src/NewMainPage/SoundMap/AudioFiles/index.js
@@ -0,0 +1,25 @@
+import loc1 from "./Loc1.wav";
+import loc2 from "./Loc2.wav";
+import loc3 from "./Loc3.wav";
+import loc4 from "./Loc4.wav";
+import loc5 from "./Loc5.wav";
+import loc6 from "./Loc6.wav";
+import loc7 from "./Loc7.wav";
+import loc8 from "./Loc8.wav";
+import loc9 from "./Loc9.wav";
+import loc10 from "./Loc10.wav";
+import loc11 from "./Loc11.wav";
+
+export {
+    loc1,
+    loc2,
+    loc3,
+    loc4,
+    loc5,
+    loc6,
+    loc7,
+    loc8,
+    loc9,
+    loc10,
+    loc11,
+}
